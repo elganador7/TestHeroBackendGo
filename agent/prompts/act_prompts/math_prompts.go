a35bf5ec44bff54e0d912c3a37795f332a8b396d
Cameron Springer <cspringer@testhero.com>
2025-02-26 22:10:34 -0500

    Prompt tweaking

--- agent/prompts/act_prompts/math_prompts.go
+++ agent/prompts/act_prompts/math_prompts.go
@@ -7,5 +7,7 @@ var (
 		You should be creative with the numerical values you use for your questino. Try to write questions that end up producing
 		whole numbers or relatively simple fractions. The higher the difficulty, the more creative you should be. You should keep question succinct
 		and avoid including formulas unless they are not commonly used.
+
+		DO NOT include context with these questions.
 	`
 )
